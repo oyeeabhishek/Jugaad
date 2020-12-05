b3167951d7329b996007e194e0812a3721167def
oyeeabhishek <68803785+oyeeabhishek@users.noreply.github.com>
2020-12-05 11:37:05 +0530

    Add files via upload

---
+++ jugaad2/initialize.js
@@ -0,0 +1,30 @@
+// Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyDNCWWvJHPAnNQDZCrJLNJ_EGnNJWff--E",
+//     authDomain: "blogging-internship.firebaseapp.com",
+//     databaseURL: "https://blogging-internship.firebaseio.com",
+//     projectId: "blogging-internship",
+//     storageBucket: "blogging-internship.appspot.com",
+//     messagingSenderId: "281110167803"
+//   };
+//   firebase.initializeApp(config);
+
+
+ 
+
+{/* <script> */}
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCwBCNzgt4LKx_dBlWkzozYY2ke2WL_uOQ",
+    authDomain: "fir-c93d3.firebaseapp.com",
+    projectId: "fir-c93d3",
+    storageBucket: "fir-c93d3.appspot.com",
+    messagingSenderId: "1038523397011",
+    appId: "1:1038523397011:web:cc148a87b4fe1960736e66",
+    measurementId: "G-8T8PQWW82J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+{/* </script> */}
